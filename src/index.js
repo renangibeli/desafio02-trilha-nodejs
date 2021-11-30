4fb4d45fd1ba386fd1b2ebc8300fa7bd08e9c73b
Renan Gibeli <renan.gibeli@hotmail.com>
2021-11-30 14:10:25 -0300

    inclusao das respostas

--- src/index.js
+++ src/index.js
@@ -10,19 +10,68 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers
+  const user = users.find(user => user.username === username)
+
+  if(!user){
+    return response.status(404).json({ error: "Usuário não encontrado"})
+  }
+
+  request.user = user
+
+  return next()
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request
+
+  if ((user.pro === false && user.todos.length < 10) || user.pro === true){
+    return next()
+  }
+
+  return response.status(403)
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers
+	const { id } = request.params
+
+  const user = users.find(user => user.username === username)
+
+  if(!user){
+    return response.status(404).json({ error: "Usuário não encontrado"})
+  }
+
+  const isValidId = validate(id)
+
+  if(!isValidId){
+    return response.status(400).json({ error: 'Id inválido'})
+  }
+
+	const todo = user.todos.find(todo => todo.id === id)
+
+  if(!todo){
+		return response.status(404).json({ error: "To do não encontrado"})
+	}
+
+  request.user = user
+	request.todo = todo
+
+	return next()
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params
+
+  const user = users.find(user => user.id === id)
+
+  if(!user){
+    return response.status(404)
+  }
+
+  request.user = user
+
+  return next()
 }
 
 app.post('/users', (request, response) => {
